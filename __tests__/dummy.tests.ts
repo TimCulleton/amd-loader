f1a1b1e75a3ce3c6fac8d4bebe5bec6e41a510ec
Tim Culleton <mit_notelluc@hotmail.com>
2019-06-12 17:09:04 +1000

    Remove Un-needed test
    Removing as this file only existed to make the builds pass in instances of zero tests

--- __tests__/dummy.tests.ts
+++
@@ -1,8 +0,0 @@
-describe(`Dummy Tests`, () => {
-
-    // Adding Fake test to pass travisCI build
-    it(`Fake tests`, () => {
-        const test = "fake";
-        expect(test).toBe("fake");
-    });
-});
