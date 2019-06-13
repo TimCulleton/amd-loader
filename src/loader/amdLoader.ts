c899a0f2e5abdf6d04847d3b626e111f5794853b
Tim Culleton <mit_notelluc@hotmail.com>
2019-06-13 16:40:40 +1000

    Add basic Loader Scaffold

---
+++ src/loader/amdLoader.ts
@@ -0,0 +1,13 @@
+import { IAmdLoaderConfig } from "../types/amdLoaderTypes";
+import { IAmdModule } from "../types/amdModuleTypes";
+import { RequireModule } from "../types/amdModuleTypes";
+import { ConfigWrapper } from "../utils/configWrapper";
+
+export const ModuleCache: Map<string, IAmdModule> = new Map();
+
+export const LoaderConfig: ConfigWrapper<IAmdLoaderConfig> = new ConfigWrapper();
+
+
+export function requireModule(moduleId: string): Promise<IAmdModule> {
+
+}
