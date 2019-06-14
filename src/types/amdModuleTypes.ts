5523e1a10b8396e60ff52c8179e52ec9ae282672
Tim Culleton <mit_notelluc@hotmail.com>
2019-06-14 15:35:55 +1000

    Add AmdModuleV2 - Temp module to have the refactor work for AmdModule

--- src/types/amdModuleTypes.ts
+++ src/types/amdModuleTypes.ts
@@ -4,14 +4,16 @@ import { GenericFunction1 } from "./commonTypes";
 export type FactoryFn = (...deps: any[]) => any;
 
 export interface IAmdModuleConfig {
-    name?: string;
+    name: string;
     path?: string;
     dependencies?: string[];
     factory?: FactoryFn;
     loaded?: boolean;
     exports?: any;
 
-    requireModule?: RequireModule;
+    requireModule: RequireModule;
+
+    getModuleFromCache(moduleId: string): IAmdModule | undefined;
 }
 
 export interface IAmdModule {
@@ -21,9 +23,10 @@ export interface IAmdModule {
     factory: FactoryFn;
     loaded: boolean;
     exports: any;
+    requireModule: RequireModule;
 
     on(evtId: "defined" | "ready", callback: GenericFunction1<this>): GenericFunction0;
-    on(evtId: "error", callback: (e: Error) => void ): GenericFunction0;
+    on(evtId: "error", callback: (e: Error) => void): GenericFunction0;
 
     emit(evtId: "defined" | "ready"): boolean;
 
@@ -35,4 +38,21 @@ export type RequireModule = (moduleId: string) => Promise<IAmdModule>;
 export interface IRequireModule {
     (moduleId: string): Promise<IAmdModule>;
     (moduleId: string[]): Promise<IAmdModule[]>;
+}
+
+export interface IAmdModuleV2 {
+    name: string;
+    path: string;
+    dependencies: string[];
+    factory: FactoryFn;
+    loaded: boolean;
+    exports: any;
+    requireModule: RequireModule;
+
+    on(evtId: "defined" | "ready", callback: GenericFunction1<this>): GenericFunction0;
+    on(evtId: "error", callback: (e: Error) => void): GenericFunction0;
+
+    emit(evtId: "defined" | "ready"): boolean;
+
+    load(): Promise<this>;
 }
